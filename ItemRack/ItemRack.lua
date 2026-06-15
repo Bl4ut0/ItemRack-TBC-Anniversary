c220ed338acf3a95d25d6c9a8ebc1fa0d8d2113a
Alex Mammen <Bl4ut0@gmail.com>
2026-06-15 01:25:20 -0400

    Revert beta version metadata and restore Development changelog on dev

--- ItemRack/ItemRack.lua
+++ ItemRack/ItemRack.lua
@@ -39,7 +39,7 @@ local GetNumAddOns = GetNumAddOns or (C_AddOns and C_AddOns.GetNumAddOns)
 
 local wowver, wowbuild, wowbuilddate, wowtoc = GetBuildInfo()
 ItemRack.Version = GetAddOnMetadata(addonName, "Version")
-ItemRack.BuildID = "v4.40-beta2-20260615"
+ItemRack.BuildID = "Dev"
 
 -- Global Debug System
 -- Usage: ItemRack.Debug("Queue", "some message", someVar)
